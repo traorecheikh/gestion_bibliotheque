682b29807b32725d072abe91634327bb780e0fbc
HanoWhisper <atidiane741@gmail.com>
2024-06-19 14:54:32 +0000

    minor changes

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,10 +1,12 @@
-document.getElementById('togglePassword').addEventListener('click', function() {
-    var passwordInput = document.getElementById('mot_de_passe');
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        this.textContent = 'Masquer';
+document
+  .getElementById("togglePassword")
+  .addEventListener("click", function () {
+    var passwordInput = document.getElementById("mot_de_passe");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      this.textContent = "Masquer";
     } else {
-        passwordInput.type = 'password';
-        this.textContent = 'Afficher';
+      passwordInput.type = "password";
+      this.textContent = "Afficher";
     }
-});
+  });
